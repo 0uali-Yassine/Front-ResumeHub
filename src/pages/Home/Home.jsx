d8c19c69966fc4855ea8d59670dc418a0b1f4cb2
0uali-Yassine <yassinzerouali0@gmail.com>
2025-04-05 12:04:15 +0000

    test home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,22 +3,23 @@ import Navbar from "../../components/Navbar";
 import AddEditResume from "./AddEditResume";
 
 const Home = () => {
- 
-  return (
-    <section>
-      <Navbar  />
-      <div className="container mx-auto p-[2rem]">
-      
-    </div>
 
-      <button className=" w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10">
-      </button>
-      
-      <AddEditResume />
+    return (
+        <section>
+            <Navbar />
+            <div className="container mx-auto p-[2rem]">
+                <h1 className="text-4xl text-slate-900 font-semibold">Your Thoughts</h1>
+                <p className="text-slate-500 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+            </div>
 
-     
-    </section>
-  )
+            <button className=" w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10">
+            </button>
+
+            <AddEditResume />
+
+
+        </section>
+    )
 }
 
 export default Home
